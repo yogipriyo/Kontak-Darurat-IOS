4ec925b339ba1019a40f87dcb4611adef363a9ad
yogiprayogo <yogi.prayogo@go-jek.com>
2016-03-02 17:21:45 +0700

    first push

---
+++ Kontak Darurat/FirstViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.swift
+//  Kontak Darurat
+//
+//  Created by yogi prayogo on 3/2/16.
+//  Copyright © 2016 DIUI. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var NameField: UITextField!
+    @IBOutlet weak var KTPField: UITextField!
+    @IBOutlet weak var BPJSField: UITextField!
+    @IBOutlet weak var NoIbuField: UITextField!
+    @IBOutlet weak var NoBapakField: UITextField!
+    @IBOutlet weak var NoKantorField: UITextField!
+    @IBOutlet weak var NoLainField: UITextField!
+    @IBOutlet weak var BuatQRButton: UIButton!
+    @IBOutlet weak var SimpanQRButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
